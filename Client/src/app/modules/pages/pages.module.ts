81539ee5260137c937586728d08a96d31932182c
klamaj <kri.lamaj@gmail.com>
2024-01-03 15:20:59 +0200

    create home component

---
+++ Client/src/app/modules/pages/pages.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+export const pagesRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(pagesRoutes)
+  ]
+})
+export class PagesModule { }
