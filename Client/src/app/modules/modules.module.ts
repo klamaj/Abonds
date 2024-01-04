e60140261e14ff443fa5c20a1fef619a319d9448
klamaj <kri.lamaj@gmail.com>
2024-01-04 12:31:11 +0200

    Add user component for alamaj

--- Client/src/app/modules/modules.module.ts
+++ Client/src/app/modules/modules.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddUserComponent } from './add-user/add-user.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddUserComponent
+  ],
   imports: [
     CommonModule
   ]
