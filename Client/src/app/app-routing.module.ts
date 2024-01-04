fa3099b8376eae269b46c2dbcbfbfd2045270fcf
klamaj <kri.lamaj@gmail.com>
2024-01-04 12:44:08 +0200

    Login & Register Component

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/pages/auth/login/login.component';
+import { RegisterComponent } from './modules/pages/auth/register/register.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () =>  import('./modules/pages/pages.module').then(m => m.PagesModule),
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 
