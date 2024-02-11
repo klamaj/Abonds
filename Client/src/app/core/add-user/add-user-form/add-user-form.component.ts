31dbededdfe5fe15581b9277442ae1a2f8bee831
klamaj <kri.lamaj@gmail.com>
2024-02-11 13:34:48 +0200

    Complete Add User UI

--- Client/src/app/core/add-user/add-user-form/add-user-form.component.ts
+++ Client/src/app/core/add-user/add-user-form/add-user-form.component.ts
@@ -10,21 +10,29 @@ export class AddUserFormComponent implements OnInit {
 
   addUserForm: FormGroup;
 
+  private anio: number = new Date().getFullYear();  
+
   constructor() {
     this.addUserForm = this.generateAddUserForm();
   }
 
-  ngOnInit(): void {
-      
-  }
+  ngOnInit(): void {}
 
   generateAddUserForm(): FormGroup {
     return new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       name: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]')]),
-      surname: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]')])
+      surname: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]')]),
+      day: new FormControl(undefined, [Validators.required, Validators.pattern('[0-9]'), Validators.min(1), Validators.max(31)]),
+      month: new FormControl('00', [Validators.required, Validators.pattern('[0-9]')]),
+      year: new FormControl(undefined, [Validators.required, Validators.pattern('[0-9]'), Validators.max(this.anio - 18)]),
+      sex: new FormControl('male',[Validators.required]),
+      questions: new FormControl('0', [Validators.required]),
+      questionId: new FormControl('0')
     })
   }
 
-  addUser(): void {}
+  addUser(): void {
+    console.log('boom')
+  }
 }
