bd76d03021d7dd1fcffed0de8fd83b96127117a6
klamaj <kri.lamaj@gmail.com>
2024-03-27 15:32:19 +0200

    Requests UI impelmented

--- Client/src/app/core/requests/requests.component.ts
+++ Client/src/app/core/requests/requests.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-requests',
   templateUrl: './requests.component.html',
   styleUrls: ['./requests.component.scss']
 })
-export class RequestsComponent {
+export class RequestsComponent implements OnInit{
 
+  rejectForm: FormGroup;
+
+  constructor() {
+    this.rejectForm = this.generateRejectForm();
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+  generateRejectForm(): FormGroup {
+    return new FormGroup({
+      message: new FormControl(undefined, [Validators.required]),
+      status: new FormControl()
+    });
+  }
+
+  rejectUser(): void {
+
+  }
 }
